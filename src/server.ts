e84cd9c29822fd00081d28c34303bc9d72ecf517
luizcsanchez <luizcsanchezz0@gmail.com>
2024-06-20 02:06:19 -0300

    Configurando Fastify e Prisma ORM

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello Meli')
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/', () => {
+    return 'Hello Meli'
+})
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('HTTP server running!')
+})
